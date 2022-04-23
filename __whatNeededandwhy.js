8a982922c0d89d2149a35505df041b40b7a4f9fd
foadtkf <fakhrulif@gmail.com>
2022-04-23 23:27:54 +0600

    installed all dependencies

---
+++ __whatNeededandwhy.js
@@ -0,0 +1,7 @@
+// express: eita nodejs er ekta framework. eta web application building aro easy ar fast kore dey
+
+// cors: duita alada port theke communicate korbo tai 
+
+// mongodb: database er jonno
+
+// dotenv: ekta special purpose er jonno
